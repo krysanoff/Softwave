b25a31d59dd076b72587280202d227a844319094
Eugene Krysanov <eugene.krysanov@gmail.com>
2019-10-06 18:18:06 +0500

    Add active class for active year button

--- resources/js/components/Years.js
+++ resources/js/components/Years.js
@@ -4,24 +4,36 @@ import { getYear } from "../actions";
 export default class Years extends Component {
     years = JSON.parse(document.getElementById('years').dataset.years)
 
+    state = {
+        activeYear: this.years[0].year
+    }
+
     getYears() {
-        return this.years.map((item, i) =>
-            <li className="my-2" key={"year_" + i}>
-                <a href="#"
-                   role="button"
-                   className={"btn btn-outline-secondary"}
-                   onClick={(e) => this.handleYearClick(e, item.year)}>
-                    {item.year}
-                </a>
-            </li>
+        return this.years.map((item, i) => {
+                let active = ""
+                if (item.year == this.state.activeYear) {
+                    active = "active";
+                }
+
+                return(
+                    <li className="col-4 col-md-12 my-md-2" key={"year_" + i}>
+                        <a href="#"
+                           role="button"
+                           className={"btn btn-outline-secondary " + active}
+                           onClick={(e) => this.handleYearClick(e, item.year)}>
+                            {item.year}
+                        </a>
+                    </li>
+                )
+            }
         )
     }
 
     render() {
         return (
-            <div className="col-3">
+            <div className="col-12 col-md-3 order-md-3">
                 <h3>Years</h3>
-                <ul className="list-unstyled">
+                <ul className="row list-unstyled">
                     {this.getYears()}
                 </ul>
             </div>
@@ -31,5 +43,8 @@ export default class Years extends Component {
     handleYearClick(e, year) {
         e.preventDefault()
         getYear(year)
+        this.setState({
+            activeYear: year
+        })
     }
 }
